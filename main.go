1720190491ca9430108a010636a6c365bf012977
Ravi Wu <raviwu@gmail.com>
2020-07-25 10:45:21 +0800

    Pass struct as data

--- main.go
+++ main.go
@@ -12,12 +12,26 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
+type person struct {
+	Name string
+	Age  int
+}
+
 func main() {
 	ppl := map[string]int{
 		"Ravi": 35,
 		"Rene": 33,
 	}
-	err := tpl.ExecuteTemplate(os.Stdout, "passData.gohtml", ppl)
+	err := tpl.ExecuteTemplate(os.Stdout, "passMap.gohtml", ppl)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ravi := person{
+		Name: "Ravi",
+		Age:  35,
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "passStruct.gohtml", ravi)
 	if err != nil {
 		log.Fatalln(err)
 	}
